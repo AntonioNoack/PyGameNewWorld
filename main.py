66af2c14c9400c26a7589cacc6e4e69f65ee0925
Isa <Isabell.Noack@stud.eah-jena.de>
2026-03-13 17:19:47 +0100

    added player position and font

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption('New World')
+
 game_is_running = True
 clock = pygame.time.Clock()
 dt = 0
@@ -34,7 +36,13 @@ while game_is_running:
     # Move Player
     player.move(dt)
    
+    # Display Player Position
+    font = pygame.freetype.Font("assets/fonts/OpenSans-Medium.ttf", 24)
+    font_surface, _ = font.render(f"Player Position: ({player_pos.x:.0f}, {player_pos.y:.0f})", "black")
+    screen.blit(font_surface, (10, 10))
 
+
+    # Update the display with everything drawn
     pygame.display.flip()
     
     # dt is delta time in seconds since last frame, used for framerate-
