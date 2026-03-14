992643f28810ebc62c1f607bd958f10ac7af1123
Isa <Isabell.Noack@stud.eah-jena.de>
2026-03-14 16:05:38 +0100

    player orientation

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player:
         self.speed = 120
         self.player_pos = player_pos
         self.tileset = Tileset("assets/tilemaps/character_walking.png", (16,32))
+        self.orientation = "down"
 
     def move(self, dt):
         
@@ -14,32 +15,49 @@ class Player:
         if keys[pygame.K_d] and keys[pygame.K_s]:
             self.player_pos.x += self.speed * dt / 1.4
             self.player_pos.y += self.speed * dt / 1.4
+            self.orientation = "down"
         
         elif keys[pygame.K_a] and keys[pygame.K_s]:
             self.player_pos.x -= self.speed * dt / 1.4
             self.player_pos.y += self.speed * dt / 1.4
+            self.orientation = "down"
 
         elif keys[pygame.K_d] and keys[pygame.K_w]:
             self.player_pos.x += self.speed * dt / 1.4
             self.player_pos.y -= self.speed * dt / 1.4
+            self.orientation = "up"
         
         elif keys[pygame.K_a] and keys[pygame.K_w]:
             self.player_pos.x -= self.speed * dt / 1.4
             self.player_pos.y -= self.speed * dt / 1.4
+            self.orientation = "up"
         
         elif keys[pygame.K_w]:
             self.player_pos.y -= self.speed * dt
+            self.orientation = "up"
         
         elif keys[pygame.K_s]:
             self.player_pos.y += self.speed * dt
+            self.orientation = "down"
         
         elif keys[pygame.K_a]:
             self.player_pos.x -= self.speed * dt
+            self.orientation = "left"
         
         elif keys[pygame.K_d]:
             self.player_pos.x += self.speed * dt
+            self.orientation = "right"
 
     def draw(self, screen):
         #pygame.draw.circle(screen, "black", self.player_pos, 40)
+        
+        if self.orientation == "left":
+            tile = self.tileset.tiles[3]
+        elif self.orientation == "right":
+            tile = self.tileset.tiles[1]
+        elif self.orientation == "up":
+            tile = self.tileset.tiles[2]
+        elif self.orientation == "down":
+            tile = self.tileset.tiles[0]
 
-        screen.blit(self.tileset.tiles[0], (self.player_pos.x-16/2, self.player_pos.y-32/2))
+        screen.blit(tile, (self.player_pos.x-16/2, self.player_pos.y-32/2))
